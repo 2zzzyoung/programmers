0bda2ae379b8f3485d21057ba587f697b99b7c63
2zzzyoung <mzooyoungn@gmail.com>
2023-12-11 23:53:47 +0900

    특별한 이차원 배열 1

---
+++ level_0/특별한 이차원 배열 1.js
@@ -0,0 +1,30 @@
+`정수 n이 매개변수로 주어질 때, 다음과 같은 n × n 크기의 이차원 배열 arr를 return 하는 solution 함수를 작성해 주세요.
+
+arr[i][j] (0 ≤ i, j < n)의 값은 i = j라면 1, 아니라면 0입니다.`;
+
+function solution(n) {
+  let answer = [[]];
+  for (let i = 0; i < n; i++) {
+    // answer[i] = []
+    for (let j = 0; j < n; j++) {
+      if (i === j) {
+        answer[i][j] = 1;
+      } else {
+        answer[i][j] = 0;
+      }
+    }
+  }
+  return answer;
+}
+
+// 다른 사람의 풀이
+
+function solution(n) {
+  const answer = Array.from(Array(n), () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    answer[i][i] = 1;
+  }
+
+  return answer;
+}
